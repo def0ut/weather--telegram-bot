fd20eb8c4f558ea04a126348df9cc01ed55840e3
Эмиль <112210390+def0ut@users.noreply.github.com>
2022-10-08 20:32:10 +0300

    Сhanged the structure of the BOT code.
    I made: 
    So that you can input the city, and URL changes to the NAME of the city.

---
+++ weather-telegram-bot.py
@@ -0,0 +1,53 @@
+
+# ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И ПАКЕТОВ
+
+import telebot
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+import config
+
+from aiogram import types
+
+# ПОДКЛЮЧЕНИЕ ТОКЕНА ДЛЯ ИСПОЛЬЗОВАНИЯ БОТА В ТЕЛЕГРАМ
+
+bot = telebot.TeleBot(config.TOKEN)
+
+# КОДИНГ БОТА
+
+@bot.message_handler(commands=['start'])
+
+def GET_START_INFORMATION(message):
+
+    bot.send_message(message.chat.id,
+                    f'Привет, {message.from_user.first_name}. 🙋🏻‍♂\n'
+                    f'Я мини-функциональный БОТ, который показывает ПОГОДУ.\n\n'
+                    
+                    f'Чтобы продолжить введите любой город.'
+
+    )
+
+@bot.message_handler(content_types=['text'])
+
+def GET_WEATHER_IN_CITIES(message):
+
+    URL = f'https://sinoptik.ua/погода-{message.text.lower()}'
+    r = requests.get(URL)
+    HTML = BeautifulSoup(r.content, 'html.parser')
+    for ELEMENTS in HTML.select('#content'):
+        MINIMAL_TEMP = ELEMENTS.select('.min')[0].text
+        MAXIMUM_TEMP = ELEMENTS.select('.max')[0].text
+        CURRENT_TEMP = ELEMENTS.find('p', {'class':'today-temp'}).string.strip()
+
+        bot.send_message(
+            message.chat.id, f'''☁️ Погода в городе {("<b><u>" + message.text.upper() + "</u></b>")} на сегодня:\n\n'''
+                             f'''• Минимальная температура: {str(MINIMAL_TEMP)[4:]}\n'''
+                             f'''• Максимальная температура: {str(MAXIMUM_TEMP)[5:]}\n'''
+                             f'''• Текущая температура: {str(CURRENT_TEMP)[:-1]}''', 
+                             
+                             parse_mode = "HTML"                             
+                        )
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
